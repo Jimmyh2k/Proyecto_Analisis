3cd98ef5c46765e9f9bd66344d0275b561be13f0
Braulio Rodríguez <bjrp1999@hotmail.com>
2020-09-25 22:55:45 -0600

    Implementación de Inicio de sesión y registro

---
+++ Proyecto_Analisis.UI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Proyecto_Analisis.UI.Areas.Identity.Data;
+using Proyecto_Analisis.UI.Data;
+
+[assembly: HostingStartup(typeof(Proyecto_Analisis.UI.Areas.Identity.IdentityHostingStartup))]
+namespace Proyecto_Analisis.UI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<UsuariosDeProgramaDeFacturacion>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
